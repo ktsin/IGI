cddb3e88b84dd31f2cf60330e36b55ea5aae8443
Pavel Kazutsin <43731609+ktsin@users.noreply.github.com>
2021-04-16 11:15:18 +0300

    0-

--- WpfApp2/MainWindowViewModel.cs
+++ WpfApp2/MainWindowViewModel.cs
@@ -1,15 +1,29 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace WpfApp
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        public MainWindowViewModel() { }
+        public MainWindowViewModel(MainWindowModel model)
+        {
+            this._model = model;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
+
+        [Bindable(BindableSupport.Yes)]
+        [ListBindable(true)]
+        public List<string> TableNames { get => _model.TableNames; }
+
+        private readonly MainWindowModel _model = null;
+
     }
 }
